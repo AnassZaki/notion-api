fec74fb1f0f2b4090f105732a7f4775343a9badd
AnassZaki <Thezaki.anass@gmail.com>
2021-05-27 00:55:00 +0200

    ❌  proper error handling

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,50 @@
 import { Request } from "@hapi/hapi";
 import { Client } from "@notionhq/client";
+import chalk from "chalk";
 
 import filter from "../helpers/filter";
 
 const Notion = new Client({ auth: process.env.NOTION_SECRET_KEY });
 
-interface Payload {
-  id: string;
-}
+export const getDatabaseById = async (req: Request) => {
+  const { id: database_id } = <{ id: string }>req.payload;
+
+  try {
+    const response = await Notion.databases.retrieve({
+      database_id,
+    });
+
+    return { ...response };
+  } catch (_error) {
+    const error = new Error(
+      `Error retrieving database ${chalk.magenta(database_id)}: ${
+        _error.message
+      }`
+    );
+
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getDatabasePages = async (req: Request) => {
+  const { id: database_id } = <{ id: string }>req.payload;
+
+  try {
+    const response = await Notion.databases.query({
+      database_id,
+    });
+
+    return { ...response };
+  } catch (_error) {
+    const error = new Error(
+      `Error querying database ${chalk.magenta(database_id)}: ${_error.message}`
+    );
+
+    console.error(error);
+    throw error;
+  }
+};
 
 interface InsertionPayload {
   database_id: string;
@@ -15,71 +52,62 @@ interface InsertionPayload {
   status?: string;
 }
 
-export const getDatabaseById = async (req: Request) => {
-  const payload = <Payload>req.payload;
-
-  const response = await Notion.databases.retrieve({
-    database_id: payload.id,
-  });
-
-  return { ...response };
-};
-
-export const getDatabasePages = async (req: Request) => {
-  const payload = <Payload>req.payload;
-
-  const response = await Notion.databases.query({
-    database_id: payload.id,
-  });
-
-  return { ...response };
-};
-
 export const insertPage = async (req: Request) => {
   const { database_id, page_title } = <InsertionPayload>req.payload;
 
-  const database = await Notion.databases.retrieve({
-    database_id,
-  });
+  try {
+    const database = await Notion.databases.retrieve({
+      database_id,
+    });
 
-  const {
-    title: { name, id },
-  } = filter(database.properties, ["title"]);
+    const {
+      title: { name, id },
+    } = filter(database.properties, ["title"]);
 
-  const response = await Notion.pages.create({
-    parent: { database_id },
-    properties: {
-      [name]: {
-        id,
-        type: "title",
-        title: [
-          {
-            text: { content: page_title },
-            plain_text: page_title,
-            type: "text",
-            annotations: {
-              bold: false,
-              italic: false,
-              strikethrough: false,
-              underline: false,
-              code: false,
-              color: "default",
+    const response = await Notion.pages.create({
+      parent: { database_id },
+      properties: {
+        [name]: {
+          id,
+          type: "title",
+          title: [
+            {
+              text: { content: page_title },
+              plain_text: page_title,
+              type: "text",
+              annotations: {
+                bold: false,
+                italic: false,
+                strikethrough: false,
+                underline: false,
+                code: false,
+                color: "default",
+              },
             },
-          },
-        ],
+          ],
+        },
+        // TODO: add ability to select issue status
+        // Status: {
+        //   id: "status",
+        //   type: "select",
+        //   select: {
+        //     id: "0082af65-d0ca-4f39-85bd-032a2e5f7a58",
+        //     name: "Backlog",
+        //     color: "pink",
+        //   },
+        // },
       },
-      // TODO: add ability to select issue status
-      // Status: {
-      //   id: "status",
-      //   type: "select",
-      //   select: {
-      //     id: "0082af65-d0ca-4f39-85bd-032a2e5f7a58",
-      //     name: "Backlog",
-      //     color: "pink",
-      //   },
-      // },
-    },
-  });
+    });
 
-  return { ...response };
+    return { ...response };
+  } catch (_error) {
+    const error = new Error(
+      `Error creating page in database ${chalk.magenta(database_id)}: ${
+        _error.message
+      }`
+    );
+
+    console.error(error);
+    throw error;
+  }
 };
--- src/services/slack/index.ts
+++ src/services/slack/index.ts
@@ -1,5 +1,6 @@
 import { WebClient } from "@slack/web-api";
 import axios from "axios";
+import Boom from "@hapi/boom";
 
 import { config } from "dotenv";
 import { getTextBlock } from "./config";
@@ -11,6 +12,19 @@ const client = new WebClient(process.env.SLACK_APP_TOKEN);
 export const getUserIdByEmail = async (email: string) => {
   try {
     const result = await client.users.lookupByEmail({ email });
+
+    if (!result.user) {
+      const message = `User ID not found from email: ${email}`;
+
+      Boom.notFound(message);
+
+      return {
+        ok: false,
+        error: {
+          message,
+        },
+      };
+    }
 
     return {
       ok: result.ok,
@@ -30,6 +44,7 @@ export const postSlackMessage = async (userId: string) => {
     })
     .catch((_error) => {
       const error = new Error(`Error posting slack message: ${_error.message}`);
+
       console.error(error);
       throw error;
     });
